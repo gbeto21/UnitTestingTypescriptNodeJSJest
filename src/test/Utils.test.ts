7cb7a2a934b2903a7a505a9d9905c3dbadc7abb3
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-03-19 13:25:40 -0600

    More tests were added to the Utils.test file.

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,4 +1,4 @@
-import { toUpperCase } from "../app/Utils"
+import { getStringInfo, toUpperCase } from "../app/Utils"
 
 describe("Utils test suite", () => {
   it("should return uppercase of valid string", () => {
@@ -7,9 +7,40 @@ describe("Utils test suite", () => {
     const expected = "ABC"
 
     //act:
-    const actual = toUpperCase("abc")
+    const actual = sut("abc")
 
     //assert:
     expect(actual).toBe(expected)
+  })
+
+  it.only("should return info for valid string", () => {
+    const actual = getStringInfo("My-String")
+
+    expect(actual.lowerCase).toBe("my-string")
+    expect(actual.extraInfo).toEqual({})
+
+    expect(actual.characters.length).toBe(9)
+    expect(actual.characters).toHaveLength(9)
+
+    expect(actual.characters).toEqual([
+      "M",
+      "y",
+      "-",
+      "S",
+      "t",
+      "r",
+      "i",
+      "n",
+      "g",
+    ])
+    expect(actual.characters).toContain<string>("M")
+    expect(actual.characters).toEqual(
+      expect.arrayContaining(["S", "t", "r", "i", "n", "g", "M", "y", "-"])
+    )
+
+    expect(actual.extraInfo).not.toBe(undefined)
+    expect(actual.extraInfo).not.toBeUndefined()
+    expect(actual.extraInfo).toBeDefined()
+    expect(actual.extraInfo).toBeTruthy()
   })
 })
