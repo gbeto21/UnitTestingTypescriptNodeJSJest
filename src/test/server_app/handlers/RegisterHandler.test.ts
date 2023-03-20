42071140b66a3cd01e2d003c19be48619bf1cf40
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-03-20 11:44:53 -0600

    The Reservation and login handler test were added.

--- src/test/server_app/handlers/RegisterHandler.test.ts
+++ src/test/server_app/handlers/RegisterHandler.test.ts
@@ -1,68 +1,98 @@
+import { Authorizer } from "../../../app/server_app/auth/Authorizer";
 import { RegisterHandler } from "../../../app/server_app/handlers/RegisterHandler"
-import { IncomingMessage, ServerResponse } from "http"
-import { Authorizer } from "../../../app/server_app/auth/Authorizer"
-import {
-  HTTP_CODES,
-  HTTP_METHODS,
-} from "../../../app/server_app/model/ServerModel"
-import { Account } from "../../../app/server_app/model/AuthModel"
+import { IncomingMessage, ServerResponse } from "http";
+import { HTTP_CODES, HTTP_METHODS } from "../../../app/server_app/model/ServerModel";
+import { Account } from "../../../app/server_app/model/AuthModel";
 
-const getRequestBodyMock = jest.fn()
+const getRequestBodyMock = jest.fn();
 
-jest.mock("../../../app/server_app/utils/Utils", () => ({
-  getRequestBody: () => getRequestBodyMock(),
+jest.mock('../../../app/server_app/utils/Utils', ()=>({
+    getRequestBody: () => getRequestBodyMock()
 }))
 
-describe("RegisterHandler test suite", () => {
-  let sut: RegisterHandler
+describe('RegisterHandler test suite', ()=>{
 
-  const request = {
-    method: undefined,
-  }
+    let sut: RegisterHandler;
 
-  const responseMock = {
-    statusCode: 0,
-    writeHead: jest.fn(),
-    write: jest.fn(),
-  }
+    const request = {
+        method: undefined
+    }
 
-  const authorizerMock = {
-    registerUser: jest.fn(),
-  }
+    const responseMock = {
+        statusCode: 0,
+        writeHead: jest.fn(),
+        write: jest.fn()
+    }
 
-  const someAccount: Account = {
-    id: "",
-    password: "somePassword",
-    userName: "someUserName",
-  }
+    const authorizerMock = {
+        registerUser: jest.fn()
+    }
 
-  const someId = "1234"
+    const someAccount: Account = {
+        id: '',
+        password: 'somePassword',
+        userName: 'someUserName'
+    }
 
-  beforeEach(() => {
-    sut = new RegisterHandler(
-      request as IncomingMessage,
-      responseMock as any as ServerResponse,
-      authorizerMock as any as Authorizer
-    )
-  })
+    const someId = '1234';
 
-  afterEach(() => {
-    jest.clearAllMocks()
-  })
+    beforeEach(()=>{
+        sut = new RegisterHandler(
+            request as IncomingMessage,
+            responseMock as any as ServerResponse,
+            authorizerMock as any as Authorizer
+        )
+    });
 
-  it("should register valid account in request", async () => {
-    request.method = HTTP_METHODS.POST
-    getRequestBodyMock.mockResolvedValueOnce(someAccount)
-    authorizerMock.registerUser.mockResolvedValueOnce(someId)
-
-    await sut.handleRequest()
-
-    expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED)
-    expect(responseMock.writeHead).toBeCalledWith(HTTP_CODES.CREATED, {
-      "Content-Type": "application/json",
+    afterEach(()=>{
+        jest.clearAllMocks();
     })
-    expect(responseMock.write).toBeCalledWith(
-      JSON.stringify({ userId: someId })
-    )
-  })
+
+    it('should register valid accounts in requests', async ()=>{
+        request.method = HTTP_METHODS.POST;
+        getRequestBodyMock.mockResolvedValueOnce(someAccount);
+        authorizerMock.registerUser.mockResolvedValueOnce(someId);
+
+        await sut.handleRequest();
+
+        expect(responseMock.statusCode).toBe(HTTP_CODES.CREATED);
+        expect(responseMock.writeHead).toBeCalledWith(
+            HTTP_CODES.CREATED,
+            { 'Content-Type': 'application/json' }
+        )
+        expect(responseMock.write).toBeCalledWith(
+            JSON.stringify({
+                userId: someId
+            })
+        )
+    })
+
+    it('should not register invalid accounts in requests', async () => {
+        request.method = HTTP_METHODS.POST;
+        getRequestBodyMock.mockResolvedValueOnce({});
+
+        await sut.handleRequest();
+
+        expect(responseMock.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
+        expect(responseMock.writeHead).toBeCalledWith(
+            HTTP_CODES.BAD_REQUEST, { 'Content-Type': 'application/json' }
+        )
+        expect(responseMock.write).toBeCalledWith(
+            JSON.stringify(
+                'userName and password required'
+            )
+        )
+    });
+
+    it('should do nothing for not supported http methods', async () => {
+        request.method = HTTP_METHODS.GET;
+        await sut.handleRequest();
+
+        expect(responseMock.writeHead).not.toBeCalled();
+        expect(responseMock.write).not.toBeCalled();
+        expect(getRequestBodyMock).not.toBeCalled();
+    });
+
+
+
 })
