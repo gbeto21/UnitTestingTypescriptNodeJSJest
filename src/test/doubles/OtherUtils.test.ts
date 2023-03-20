fc5ee6a9bb9a8603d5ab2972918100f3c1589ae8
Alberto Gonzalez <albertoglezb@hotmail.com>
2023-03-20 07:39:44 -0600

    The mock tests were implemented in 2 different ways.

--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -4,9 +4,56 @@ import {
 } from "../../app/doubles/OtherUtils"
 
 describe("OtherUtils test suite", () => {
-  it("ToUpperCase - calls callback for invalid argument", () => {
-    const actual = toUpperCaseWithCb("", () => {})
-    expect(actual).toBeUndefined()
+  describe.only("Tracking callbacks with Jest mocks", () => {
+    const callBackMock = jest.fn()
+
+    afterEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it("calls callback for invalid argument - tracks calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock)
+      expect(actual).toBeUndefined()
+      expect(callBackMock).toBeCalledWith("Invalid argument!")
+      expect(callBackMock).toBeCalledTimes(1)
+    })
+
+    it("calls callback for valid argument - tracks calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock)
+      expect(actual).toBe("ABC")
+      expect(callBackMock).toBeCalledWith("called function with abc")
+      expect(callBackMock).toBeCalledTimes(1)
+    })
+  })
+
+  describe.only("Tracking callbacks", () => {
+    let cbArgs = []
+    let timesCalled = 0
+
+    function callBackMock(arg: string) {
+      cbArgs.push(arg)
+      timesCalled++
+    }
+
+    afterEach(() => {
+      // clearing tracking fiels
+      cbArgs = []
+      timesCalled = 0
+    })
+
+    it("calls callback for invalid argument - tracks calls", () => {
+      const actual = toUpperCaseWithCb("", callBackMock)
+      expect(actual).toBeUndefined()
+      expect(cbArgs).toContain("Invalid argument!")
+      expect(timesCalled).toBe(1)
+    })
+
+    it("calls callback for valid argument - tracks calls", () => {
+      const actual = toUpperCaseWithCb("abc", callBackMock)
+      expect(actual).toBe("ABC")
+      expect(cbArgs).toContain("called function with abc")
+      expect(timesCalled).toBe(1)
+    })
   })
 
   it("ToUpperCase - calls callback for valid argument", () => {
